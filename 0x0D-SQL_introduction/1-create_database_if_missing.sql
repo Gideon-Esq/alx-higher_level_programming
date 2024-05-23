66cbbb8b5d3d4565951ec25c5720c74386ee4a5e
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-05-23 01:15:46 -0700

    Creates the database hbtn_0c_0 in MySQL server.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
 
 -- Creates a database name htbn_0c_0
--- Thos code must not fail if database exist.
-CREATE database hbtn_0c_0 IF NOT EXISTS
+-- This code must not fail if database exist.
+CREATE DATABASE hbtn_0c_0 IF NOT EXISTS
+
