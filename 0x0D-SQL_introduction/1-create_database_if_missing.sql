79bb34b706f54fa33729590a7fb540e8fad8d9b6
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-05-24 02:06:48 -0700

    Deletes the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
 
--- Creates a database name htbn_0c_0
+-- Creates a database name hbtn_0c_0
 -- This code must not fail if database exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
 
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Delete the database hbtn_0c_0 in MYSQL server.
+-- if hbtn_0c_0 doesn't exist, delete hbtn_0c_0.
+DROP DATABASE IF EXISTS hbtn_0c_0
