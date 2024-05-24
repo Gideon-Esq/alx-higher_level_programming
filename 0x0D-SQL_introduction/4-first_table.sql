17efb5e755596f699ee3daa12150474280c36b97
Ayooluwa Gideon Oloyede <ayooluwaoloyede71@gmail.com>
2024-05-24 10:19:17 -0700

    Creates a table called first_table in the current database in your MySQL server.

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table called first_table in the current database in your MySQL server.
+-- first_table description:
+-- id int
+-- name Varchar(256)
+CREATE TABLE IF NOT EXISTS first_table (id int, name VARCHAR(256));
