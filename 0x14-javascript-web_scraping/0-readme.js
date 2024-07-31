b1258dc68c0dd4faa1f391b1ac3d099244dee346
Ayooluwa Gideon OLOYEDE <ayooluwaoloyede71@gmail.com>
2024-07-31 01:40:07 -0700

    Prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', function (err, data) {
-	if (data === undefined) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+  if (data === undefined) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
