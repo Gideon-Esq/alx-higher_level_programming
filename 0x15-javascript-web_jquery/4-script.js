c95098c41f656c9860a791d4af15251068badf44
Gideon-Esq <138716971+Gideon-Esq@users.noreply.github.com>
2024-08-07 23:36:26 +0100

    0x15-javascript-web_jquery updated.

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+/*Write a JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:*/
+/*$("DIV#toggle_header").click(function (toggleRed)
+        if($("header").hasClass("green"))
+            ($"header").removeClass("green");
+        else 
+            $("header").addClass("red");
+    })*/
+  $("DIV#toggle_header").click(function () {
+    $("header").toggleClass("red green");
+  });
