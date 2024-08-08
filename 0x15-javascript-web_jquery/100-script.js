acd784ade8d3f62afdb9b1f59ef6242aeefa79a0
Gideon-Esq <138716971+Gideon-Esq@users.noreply.github.com>
2024-08-08 02:49:21 +0100

    Completed advance task.

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    const colorChange = document.querySelector("header");
+    colorChange.style.color = "#FF0000";
+});
+
+
+
+
+/*const colorChange = document.querySelector("header");
+colorChange.style.color = "#FF0000";*/
