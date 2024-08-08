9719f1a65d10d264165ed60120ed8be7b32cd012
Ayooluwa Gideon OLOYEDE <ayooluwaoloyede71@gmail.com>
2024-08-07 19:00:23 -0700

    A JavaScript script that adds the c lass red to the <header> element when the u ser clicks on the tag DIV#red_header

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,4 +1,4 @@
-/*Write a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header*/
+/*A JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header*/
 $("DIV#red_header").click(function() {
     $("header").addClass("red");
 })
